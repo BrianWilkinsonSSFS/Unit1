60595804093eec5caaa0e59803aca6dc56871a3d
Christine <Christine@MacBook-Pro-2.local>
2020-07-21 12:10:07 -0400

    Initial commit

---
+++ src/Sentence.java
@@ -0,0 +1,25 @@
+/*
+  Description:
+  Write a program that will print this sentence 
+  
+  "It is a great time to be a computer programmer!" 
+  
+  on three separate lines.
+  
+  Grading Criteria:
+  Output must match the lines below:
+  
+  It is a great 
+  time to be a 
+  computer programmer!
+  
+*/
+
+public class Sentence {
+
+    public static void main(String[] args) {
+        //first output statement
+        //second output statement
+        //third output statement
+    }
+}
---
+++ src/TimeAndDate.java
@@ -0,0 +1,21 @@
+/*
+  Description:
+  Write a program that will print the 
+  current time hh:mm and the current date mm/dd/yyyy 
+  each on a separate line. Use string literals for the time and date.
+  
+  Grading Criteria:
+  Any combination of output statements is acceptable
+  as long as two lines are output and
+  the time and date are current for when the 
+  student completes the assignment.
+*/
+
+public class TimeAndDate {
+
+    public static void main(String[] args) {
+        //output current time
+        //output current date
+    }
+
+}
